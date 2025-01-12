b4f651a1a486a68645360657854e252fbfcfaa1c
Nathanael Lu <lunathanael@gmail.com>
2025-01-12 11:30:05 -0500

    up timesteps

--- src/trainer.py
+++ src/trainer.py
@@ -44,7 +44,7 @@ class Args:
     # Algorithm specific arguments
     env_id: str = "utac-v0"
     """the id of the environment"""
-    total_timesteps: int = 1_000_000
+    total_timesteps: int = 10_000_000
     """total timesteps of the experiments"""
     learning_rate: float = 2.5e-4
     """the learning rate of the optimizer"""
