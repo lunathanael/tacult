b9955c74f1542e2d79a53fdfb0452b294fa65ab4
Nathanael Lu <lunathanael@gmail.com>
2025-01-12 13:37:54 -0500

    got mcts now

--- test.py
+++ test.py
@@ -3,11 +3,14 @@ import numpy as np
 import gymnasium as gym
 import utac
 
-pth = "models/utac-v0__trainer__1__1736677780.pth"
+pth = "models/utac-v0__opp_model_1__1__1736704754.pth"
 
 env = gym.make("utac-v0")
 agent = Agent.load(pth, np.prod(env.observation_space.shape), env.action_space.n)
 
+pth = "models/utac-v0__opp_self__1__1736703452.pth"
+agent2 = Agent.load(pth, np.prod(env.observation_space.shape), env.action_space.n)
 
-evaluation = evaluate.evaluate_random(agent, num_episodes=100)
+
+evaluation = evaluate.evaluate(agent, agent2, num_episodes=100)
 print(evaluation)
