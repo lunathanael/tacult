4063db47593435c61fca24b16678c21225be028d
Nathanael Lu <lunathanael@gmail.com>
2025-01-12 02:55:27 -0500

    should be good

--- src/utacenv/__init__.py
+++ src/utacenv/__init__.py
@@ -2,6 +2,8 @@ from gymnasium.envs.registration import register
 from .envs import UtacEnv
 
 register(
-    id="utacenv/UtacEnv-v0",
-    entry_point="utacenv.envs:UtacEnv",
+    id="UtacEnv-v0",
+    entry_point="utacenv.envs.utacenv:UtacEnv",
 )
+
+__all__ = ["UtacEnv"]
