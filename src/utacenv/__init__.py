0b583c19836864befb7720d3db6e59eea36f7e5c
Nathanael Lu <lunathanael@gmail.com>
2025-01-12 03:07:42 -0500

    new reqs

--- src/utacenv/__init__.py
+++
@@ -1,9 +0,0 @@
-from gymnasium.envs.registration import register
-from .envs import UtacEnv
-
-register(
-    id="UtacEnv-v0",
-    entry_point="utacenv.envs.utacenv:UtacEnv",
-)
-
-__all__ = ["UtacEnv"]
--- src/utacenv/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from .utacenv import UtacEnv
