ad88816da4a44a32a58cd19b9a93e3217a0afc1d
Nathanael Lu <lunathanael@gmail.com>
2025-01-14 02:17:09 -0500

    new network

--- src/network.py
+++ src/network.py
@@ -2,14 +2,11 @@ import sys
 sys.path.append('..')
 
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Reshape, Activation, BatchNormalization, Conv2D, Flatten, Dropout, Dense
+from tensorflow.keras.layers import Input, BatchNormalization, Conv2D, Flatten, Dense, Add, LeakyReLU
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.mixed_precision import set_global_policy
 import tensorflow as tf
 
-"""
-Inspiration from the TicTacToeNNet by Evgeny Tyurin
-"""
 class UtacNNet():
     def __init__(self, args):
         # Enable mixed precision training
@@ -30,18 +27,42 @@ class UtacNNet():
         # Neural Net
         self.input_boards = Input(shape=(self.board_x, self.board_y, self.board_z))
 
-        x_image = Reshape((self.board_x, self.board_y, self.board_z))(self.input_boards)                # batch_size  x board_x x board_y x 1
-        h_conv1 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='same')(x_image)))         # batch_size  x board_x x board_y x num_channels
-        h_conv2 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='same')(h_conv1)))         # batch_size  x board_x x board_y x num_channels
-        h_conv3 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='same')(h_conv2)))        # batch_size  x (board_x) x (board_y) x num_channels
-        h_conv4 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='valid')(h_conv3)))        # batch_size  x (board_x-2) x (board_y-2) x num_channels
+        # Convolution layers
+        x = Conv2D(128, 3, padding='same')(self.input_boards)
+        x = BatchNormalization()(x)
+        x = LeakyReLU()(x)
 
-        h_conv4_flat = Flatten()(h_conv4)       
-        s_fc1 = Dropout(args.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(1024)(h_conv4_flat))))  # batch_size x 1024
-        s_fc2 = Dropout(args.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(512)(s_fc1))))          # batch_size x 1024
+        # Multiple residual blocks to process board patterns
+        for _ in range(8):
+            x = self.residual_block(x, 128)
 
-        self.pi = Dense(self.action_size, activation='softmax', name='pi')(s_fc2)   # batch_size x self.action_size
-        self.v = Dense(1, activation='tanh', name='v')(s_fc2)                    # batch_size x 1
+        # Final convolution to increase channels
+        x = Conv2D(256, 3, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = LeakyReLU()(x)
+        
+        # Flatten for policy and value heads
+        x = Flatten()(x)
+
+        # Policy head
+        self.pi = Dense(self.action_size, activation='softmax', name='pi')(x)
+
+        # Value head
+        self.v = Dense(256, activation='relu')(x)
+        self.v = Dense(1, activation='tanh', name='v')(self.v)
 
         self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
-        self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=Adam(args.lr))
+        self.model.compile(
+            loss=['categorical_crossentropy', 'mean_squared_error'], 
+            optimizer=Adam(args.lr)
+        )
+
+    def residual_block(self, x, filters):
+        """Simple residual block"""
+        y = Conv2D(filters, 3, padding='same')(x)
+        y = BatchNormalization()(y)
+        y = LeakyReLU()(y)
+        y = Conv2D(filters, 3, padding='same')(y)
+        y = BatchNormalization()(y)
+        out = Add()([x, y])
+        return LeakyReLU()(out)
